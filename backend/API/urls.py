9657c274992c95b65370879012cd3e0149fb8b5a
GHOST100919 <ladejebijoshua@gmail.com>
2026-05-12 17:24:11 +0100

    deployment fix

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('jobs/<uuid:pk>', JobDetailView.as_view(), name='job-detail'),
     path('users', UserListView.as_view(), name='user-list'),
     path('users/<int:pk>', UserDetailView.as_view(), name='user-detail'),
-path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='auth_token_refresh'),
+]
     
